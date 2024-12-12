385763758851bd3ac0af0f16dac3cf98f26a3b9b
Sridhar <sriisridhar81@gmail.com>
2024-12-12 16:02:45 +0530

    4

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,7 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
+  { name: "Approach", link: "#approach" },
   { name: "Contact", link: "#contact" },
 ];
 
